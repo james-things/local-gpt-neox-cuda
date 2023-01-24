284f513f2888be8bc55ae4b24a7df36c859f5583
James <jharr651@students.kennesaw.edu>
2023-01-24 03:02:34 -0500

    add script

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # local-gpt-neox-cuda
- An simple example python script demonstrating a means of running GPT-NeoX-2.7B inference locally on a single GPU
+
+ An simple example python script demonstrating a means of running GPT-NeoX-2.7B inference locally on a single GPU.
---
+++ gpt-neox-test.py
@@ -0,0 +1,16 @@
+# example script to run (a locally downloaded copy of) gpt-neox on a cuda-based gpu such as the 1080ti
+# used in example: https://huggingface.co/EleutherAI/gpt-neo-2.7B
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch    
+
+model = AutoModelForCausalLM.from_pretrained("./gpt-neo-2.7B", revision="float32", torch_dtype=torch.float32, device_map="auto", low_cpu_mem_usage=True) # low_cpu_mem_usage=False
+tokenizer = AutoTokenizer.from_pretrained("./gpt-neo-2.7B")
+
+context = """Text-based generative AI is"""
+
+input_ids = tokenizer(context, return_tensors="pt").input_ids
+input_ids = input_ids.to('cuda')
+gen_tokens = model.generate(input_ids, do_sample=True, temperature=0.9, max_length=100)
+gen_text = tokenizer.batch_decode(gen_tokens)[0]
+print(f"\nPrompt: {context}\n")
+print(f"Output: {gen_text}")
